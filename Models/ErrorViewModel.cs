49950946ce7dc0d893e5221bd62be0f2ee824f3e
Vedaiyappan Suruttaiyan <vedaiyappans@stellarit.com>
2020-07-27 12:52:46 +0530

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Leave_management.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
